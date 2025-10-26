8c7720c7106f637bf436c0022a6ee8e0426c1f46
IsaiasMr2000 <isaiasmrolon2000@gmail.com>
2025-10-26 14:20:31 -0400

    feat: create profile page, cart page and cart-item component

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
   },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/cart/cart.page').then( m => m.CartPage)
+  },
+
 ];
