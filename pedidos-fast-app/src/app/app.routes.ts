d114eef3830dee1ed45dc3dc58e3c1d68b8188af
IsaiasMr2000 <isaiasmrolon2000@gmail.com>
2025-10-26 14:02:11 -0400

    feat: create login page structure and components

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -5,4 +5,24 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/cart/cart.page').then( m => m.CartPage)
+  },
+  {
+    path: 'order-history',
+    loadComponent: () => import('./pages/order-history/order-history.page').then( m => m.OrderHistoryPage)
+  },
 ];
