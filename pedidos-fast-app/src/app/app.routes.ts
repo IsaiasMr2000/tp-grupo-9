1189c956924611fbc51ba6760c800327f9935801
IsaiasMr <110064523+IsaiasMr2000@users.noreply.github.com>
2025-10-26 14:09:51 -0400

    Revert "feat: create order-history page and shared components"

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -4,9 +4,5 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },  {
-    path: 'order-history',
-    loadComponent: () => import('./pages/order-history/order-history.page').then( m => m.OrderHistoryPage)
   },
-
 ];
