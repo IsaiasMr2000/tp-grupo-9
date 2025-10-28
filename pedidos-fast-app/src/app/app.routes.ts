ea59cd2c4447424bfe6e908fd0edfbcbc6d55b9b
IsaiasMr2000 <isaiasmrolon2000@gmail.com>
2025-10-28 15:41:29 -0400

    feat: create order-history page and shared components

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -6,5 +6,9 @@ export const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule) },
   { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartPageModule) },
-  { path: 'order-history', loadChildren: () => import('./pages/order-history/order-history.module').then(m => m.OrderHistoryPageModule) },
+  { path: 'order-history', loadChildren: () => import('./pages/order-history/order-history.module').then(m => m.OrderHistoryPageModule) },  {
+    path: 'order-history',
+    loadComponent: () => import('./pages/order-history/order-history.page').then( m => m.OrderHistoryPage)
+  },
+
 ];
