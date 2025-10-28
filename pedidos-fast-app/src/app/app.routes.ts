cdd71d813bf37f2dd396b4ba9d21b5008cf2bfb4
IsaiasMr2000 <isaiasmrolon2000@gmail.com>
2025-10-28 18:01:05 -0400

    fix: guardar cambios locales antes de merge

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router'
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage)
-  },
-  {
-    path: 'cart',
-    loadComponent: () => import('./pages/cart/cart.page').then(m => m.CartPage)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
-  },
-  {
-    path: 'order-history',
-    loadComponent: () => import('./pages/order-history/order-history.page').then(m => m.OrderHistoryPage)
-  }
-];
-=======
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -36,19 +7,29 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage)
+    loadComponent: () =>
+      import('./pages/profile/profile.page').then((m) => m.ProfilePage),
   },
   {
     path: 'cart',
-    loadComponent: () => import('./pages/cart/cart.page').then(m => m.CartPage)
+    loadComponent: () =>
+      import('./pages/cart/cart.page').then((m) => m.CartPage),
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
-  }
+    loadComponent: () =>
+      import('./pages/login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'order-history',
+    loadComponent: () =>
+      import('./pages/order-history/order-history.page').then(
+        (m) => m.OrderHistoryPage
+      ),
+  },
 ];
->>>>>>> feature/home-page
