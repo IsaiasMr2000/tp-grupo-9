5b918e8e442421c64f947d193c5542f28c37acd3
IsaiasMr2000 <isaiasmrolon2000@gmail.com>
2025-11-03 00:44:54 -0400

    feat: actualizar diseño del login y agregar logo

--- pedidos-fast-app/src/app/app.routes.ts
+++ pedidos-fast-app/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
-
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
 ];
